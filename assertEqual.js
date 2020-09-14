dd52e5ddf7a72d45713890cd6990bb495fa0b2e5
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-14 21:28:02 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🛑🛑🛑Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('✅✅✅Assertion Failed: ' + actual + ' !== ' + expected);
+
+    
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,1.63645676756);
+assertEqual('true',"true");
+assertEqual('truish',"true");
+assertEqual(1,true);
+assertEqual(undefined,null);
