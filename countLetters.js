4cb0e8dc77d4fc190719aae3d6b5d0f2de1387ec
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-16 20:19:35 +0000

    Implement countLetters in Lotide Lotide Problem countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} === ${expected}`);
+
+    
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterCount = {};
+
+  for (const letter of sentence) {
+    // console.log(letter);
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+
+  return letterCount;
+};
+
+const phrase = "lighthouse in the house";
+console.log(countLetters(phrase));
+
+assertEqual(countLetters(phrase)['l'], 1); //Pass Number of l
+assertEqual(countLetters(phrase)['e'], 3); //Pass
+assertEqual(countLetters(phrase)['u'], 2); //Pass
+assertEqual(countLetters(phrase)['h'], 4); //Pass
--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,7 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
-  for(const item of allItems) {
+  for (const item of allItems) {
     // console.log(item);
     if (itemsToCount[item]) {
       if (results[item]) {
