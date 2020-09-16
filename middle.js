84cb4621901692ae3bfaa83374b259add3afdcb5
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-16 15:19:10 +0000

    flatten.js middle.js initialized

---
+++ middle.js
@@ -0,0 +1,37 @@
+const eqArrays = function(firstArray, secondArray) {
+  // variable to store boolean value
+  let arrayEquivalence = true;  //initally true for the case of two empty arrays
+  //check to see arrays are equivalent length
+  let arrayLengthEquivalence = firstArray.length === secondArray.length;
+
+  if (arrayLengthEquivalence) {
+    //Loop through each element of the arrays and compare each num
+    for (let num = 0; num < firstArray.length; num++) {
+      if (firstArray[num] === secondArray[num]) {
+        arrayEquivalence = true;
+      } else {
+        arrayEquivalence = false;
+        break;
+      }
+
+    }
+
+  } else {
+    arrayEquivalence = false;
+  }
+  return arrayEquivalence;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+
+
+  }
+};
+
+const middle = function(array) {
+  
+};
