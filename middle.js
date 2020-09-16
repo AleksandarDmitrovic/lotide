7c4f60e042fd71179208d3a1368698b5ff44edc8
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-16 16:12:44 +0000

    Implement middle Lotide Problem

--- middle.js
+++ middle.js
@@ -33,5 +33,34 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 const middle = function(array) {
-  
+  let arrayLength = array.length;
+  let arraysWithoutMiddles = (arrayLength === 0 || arrayLength === 1 || arrayLength === 2);
+  let middleIndex = 0;
+  let middleOfArray = [];
+
+  if (arraysWithoutMiddles) {
+    return middleOfArray;
+  } else if (arrayLength % 2 === 0) {
+    middleIndex = (arrayLength / 2) - 1;
+    middleOfArray = array.slice(middleIndex, middleIndex + 2);
+  } else if (arrayLength % 2 !== 0) {
+    middleIndex = Math.floor(arrayLength / 2);
+    middleOfArray = array.slice(middleIndex, middleIndex + 1);
+  } else {
+    console.log("something failed");
+  }
+
+  return middleOfArray;
+
 };
+
+console.log('EVEN TEST CASES');
+assertArraysEqual(middle([1, 2, 3, 1]), [2, 3]);  // Should Pass Even length array
+assertArraysEqual(middle([1, 2, 3, 1, 'TEN', 'hiHowAreYa', 4453, 2342]), [1, 'TEN']);  // Should Pass Even length array
+console.log('ODD TEST CASES');
+assertArraysEqual(middle(["1", "2", "3", 1, 2]), ["3"]); //  Should Pass Odd length array
+assertArraysEqual(middle(["1", 1, "2", 2, 3, "3", 'hello', -4987, 'ok']), [3]); // Should Pass Odd length array
+console.log('NO MIDDLE TEST CASES');
+assertArraysEqual(middle([]), []);  // Should Pass No Middle Length 0 Array
+assertArraysEqual(middle([1]), []);  // Should Pass No Middle Length 1 Array
+assertArraysEqual(middle([1, 2]), []);  // Should Pass No Middle Length 2 Array
