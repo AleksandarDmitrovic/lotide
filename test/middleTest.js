6b91f83243502d1bdb96fc979c71ba44c118bdcb
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-21 16:20:35 +0000

    Refactor with Mocha and Chai I tail & middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,26 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-console.log('EVEN TEST CASES');
-assertArraysEqual(middle([1, 2, 3, 1]), [2, 3]);  // Should Pass Even length array
-assertArraysEqual(middle([1, 2, 3, 1, 'TEN', 'hiHowAreYa', 4453, 2342]), [1, 'TEN']);  // Should Pass Even length array
-console.log('ODD TEST CASES');
-assertArraysEqual(middle(["1", "2", "3", 1, 2]), ["3"]); //  Should Pass Odd length array
-assertArraysEqual(middle(["1", 1, "2", 2, 3, "3", 'hello', -4987, 'ok']), [3]); // Should Pass Odd length array
-console.log('NO MIDDLE TEST CASES');
-assertArraysEqual(middle([]), 'No Middle');  // Should Pass No Middle Length 0 Array
-assertArraysEqual(middle([1]), 'No Middle');  // Should Pass No Middle Length 1 Array
-assertArraysEqual(middle([1, 2]), 'No Middle');  // Should Pass No Middle Length 2 Array
+describe('#middle', () => {
+  it('returns [2, 3] for [1, 2, 3, 1]', () => {
+    assert.deepEqual(middle([1, 2, 3, 1]), [2, 3]);
+  });
+  it("returns [1, 'TEN'] for [1, 2, 3, 1, 'TEN', 'hiHowAreYa', 4453, 2342]", () => {
+    assert.deepEqual(middle([1, 2, 3, 1, 'TEN', 'hiHowAreYa', 4453, 2342]), [1, 'TEN']);
+  });
+  it("returns ['3'] for ['1', '2', '3', 1, 2]", () => {
+    assert.deepEqual(middle(['1', '2', '3', 1, 2]), ['3']);
+  });
+  it("returns [3] for ['1', 1, '2', 2, 3, '3', 'hello', -4987, 'ok']", () => {
+    assert.deepEqual(middle(['1', 1, '2', 2, 3, '3', 'hello', -4987, 'ok']), [3]);
+  });
+  it("returns 'No Middle' for []", () => {
+    assert.deepEqual(middle([]), 'No Middle');
+  });
+  it("returns 'No Middle' for ['5']", () => {
+    assert.deepEqual(middle(['5']), 'No Middle');
+  });
+  it("returns 'No Middle' for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), 'No Middle');
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,23 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Case: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-//Test Case: Check tail function does not modify original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-//Test Case: Empty Array
-const empt = tail([]);
-assertEqual(empt[0], 3); // should fail
-
-//Test Case: One Element Array
-const oneElm = tail([1]);
-assertEqual(oneElm[0], 1);// should fail
+describe('#tail', () => {
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [6, 7] for [5, 6, 7]", () => {
+    assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [6, false] for [true, 6, false]", () => {
+    assert.deepEqual(tail([true, 6, false]), [6, false]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []);
+  });
+});
