9dd99a08c1bf812416105d203165434c6e9a620e
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-21 14:33:27 +0000

    Lotide Refactor Example head and assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -8,10 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual(1,1.63645676756);
-assertEqual('true',"true");
-assertEqual('truish',"true");
-assertEqual(1,true);
-assertEqual(undefined,null);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,1.63645676756);
+assertEqual('true',"true");
+assertEqual('truish',"true");
+assertEqual(1,true);
+assertEqual(undefined,null);
